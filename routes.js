790ef9f62cfa20fecbe5a8bf7d8a1c3aca54f594
KUCHARLAPATI VIJAYA KUMAR VARMA <vijayakumarkucharlapati@gmail.com>
2024-06-01 20:24:53 +0530

    first commit

---
+++ routes.js
@@ -0,0 +1,45 @@
+const fs = require('fs');
+const reqHandler = (req,res)=>{
+    console.log(req.url,req.headers,req.method);
+    const url = req.url;
+    const method = req.method;
+    if(url==='/'){
+        res.write('<html>');
+        res.write('<head><title>Node Server!!</title><head>');
+        res.write('<body><form action="/message" method = "POST"><input type = "text" name="message"><button type="submit">submit</button></form></body>');
+        res.write('</html>');
+        return res.end();
+    }
+    else if(url==='/message' && method==="POST"){
+        const body = [];
+        req.on('data',chunk=>{
+            console.log(chunk);
+            body.push(chunk)
+        })
+        return req.on('end',()=>{
+            const parsedBody = Buffer.concat(body).toString();
+            const message = parsedBody.split('=')[1];
+            fs.writeFile('message.txt',message,err=>{
+                res.statusCode = 302;
+                res.setHeader('Location','/');
+                return res.end();
+            })
+        })
+    }
+    res.setHeader('Content-Type','text/html');
+    res.write('<html>');
+    res.write('<head> <title>My First Page</tile></head>');
+    res.write('<body><h1>Hello from Node.js! Server</h1></body>');
+    res.write('</html>');
+    res.end();
+}
+
+module.exports = {
+    reqHandling:reqHandler,
+    someText:"Hey Hii"
+}
+
+// exports. reqHandling = reqHandler
+// exports.someText = "hey Hoo"
+
+// module.exports= reqHandler;
